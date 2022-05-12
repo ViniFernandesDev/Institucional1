68918df4d013d991d47075240b94f5805b0a9837
ViniFernandesDev <vinifernandesdeveloper@gmail.com>
2022-05-12 17:45:11 -0300

    Criação página sobre nós

---
+++ sobre_nos.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    
+     <?php include('meta_tags.php'); ?>
+        
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+<section class="title_internas sobre_nos">    
+    <div class="container">
+        <h1>SOBRE NÓS</h1>
+    </div><!--container-->
+</section><!-- title_internas -->
+
+<section class="content_sobre">
+    <div class="content_1 container">
+        <div class="txt">
+            <h2>Nome da empresa</h2>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+        </div><!--txt-->
+        
+        <div class="img">
+            <img src="assets/img/sobre_img1.png" alt="">
+        </div><!--img-->
+    </div><!--content_1-->
+
+    <div class="content_2 container">
+        <div class="img">
+            <img src="assets/img/sobre_img2.png" alt="">
+        </div><!--img-->
+        
+        <div class="txt">
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+        </div><!--txt-->
+    </div><!--content_2-->
+</section><!--content_sobre-->
+
+<?php include('footer.php'); ?>
